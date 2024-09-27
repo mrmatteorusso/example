c5f340630948c6a5f2c3921b18e0286dfff8c96c
mrmatteorusso <mrmatteorusso@gmail.com>
2024-09-27 09:27:00 +0200

    simple CRUD in web.php+edit page

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -4,10 +4,14 @@
     </x-slot:heading>
     <h2>Jobs</h2>
 
-    <h2>{{ $job['title'] }}</h2>
-    <p>Salary: {{ $job['salary'] }}</p>
+    <h2>{{ $job->title }}</h2>
+    <p>This job pays: {{ $job->salary }}</p>
 
-    <a href="/jobs">Back to Jobs</a>
+    <p class="mt-4">
+        <x-button href="/jobs/{{$job->id}}/edit">Edit job</x-button>
+    </p>
+
+        <a href="/jobs">Back to Jobs</a>
 
     {{ request()->url() }}
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
+use function Pest\Laravel\delete;
+
 Route::get('/', function () {
 
 
@@ -45,6 +47,56 @@ Route::post('/jobs', function () {
         'employer_id' => 1,
     ]);
     return redirect('/jobs');
+});
+
+
+Route::get('/jobs/{id}/edit', function ($id) {
+
+    // $job = \Illuminate\support\Arr::first($jobs, fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+Route::patch('/jobs/{id}', function ($id) {
+
+    // $job = \Illuminate\support\Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    //Validate
+    request()->validate(
+        [
+            'title' => ['required', 'min:3'],
+            'salary' => ['required'],
+        ]
+    );
+    // Authorise
+    //update the job
+    $job = Job::findOrFail($id);
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+    // $job->save();
+    $job->update(
+        [
+
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]
+    );
+    //and persist
+    //redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+Route::delete('/jobs/{id}', function ($id) {
+
+    // $job = \Illuminate\support\Arr::first($jobs, fn($job) => $job['id'] == $id);
+    // Authorise
+    $job = Job::findOrFail($id);
+    $job->delete();
+    return redirect('/jobs');
+
+
+    return view('jobs.show', ['job' => $job]);
 });
 
 Route::get('/contacts', function () {
